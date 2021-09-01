b505c5c52d14622176bbbc9ab2ad9639ad603718
Rudra <chaudhary66667@gmail.com>
2021-09-01 10:36:26 +0530

    Tweaked for readabilty

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ import pandas as pd
 # making lists out of the provided data using pandas
 data = pd.read_csv('MORSE.csv')
 letter_list = data['LETTER'].to_list()
-print(letter_list)
 morse_list = data['CODE'].to_list()
-print(morse_list)
 
 # user input
-inp_str = input("Enter a String")
+inp_str = input("Enter a String\n")
 output_list=[]
 for char in inp_str:
     index_in_char_list = letter_list.index(char)
@@ -16,4 +14,4 @@ for char in inp_str:
     output_list.append(morse_char)
 
 morse_code = "".join(output_list)
-print(morse_code)
+print(f'The Morse Code Generated is {morse_code}')
